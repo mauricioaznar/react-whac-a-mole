783504f4db0e880c3af97ce86246b340dd51be84
mauricioaznar <mauricioaznar94@gmail.com>
2022-03-23 18:19:41 -0600

    initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import CustomThemeProvider from "./components/views/custom-theme-provider";
 import {Box} from "@mui/material";
@@ -6,28 +6,76 @@ import Counter from "./components/dum/counter/counter";
 
 function App() {
     const [score, setScore] = useState(0)
+    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+    const [isFinished, setIsFinished] = useState(false)
+    const [transistor, setTransistor] = useState(0)
+    const swtchRef = useRef<boolean>(false)
+
+
+    function handleInterval() {
+        if (!isFinished) {
+            setTransistor(transistor + 1)
+            console.log(transistor, swtchRef.current)
+            if (swtchRef.current) {
+                setActiveIndex(Math.floor(Math.random() * 9))
+            } else {
+                setActiveIndex(null)
+            }
+
+
+            swtchRef.current = !swtchRef.current
+        }
+
+    }
+
+    function handleBoxOnClick (boxIndex: number) {
+        if (boxIndex === activeIndex) {
+            setActiveIndex(null)
+            setScore(score + 1)
+        }
+    }
+
+    function handleOnFinish () {
+        setIsFinished(true)
+        setActiveIndex(null)
+    }
+    
 
     return (
         <CustomThemeProvider>
+            <Counter
+                showText={false}
+                onChangeCount={handleInterval}
+                onStartCount={handleInterval}
+                defaultInterval={500}
+                initialCount={10}
+            />
             <Box sx={{
                 position: "absolute",
                 left: "10px",
                 top: "10px",
             }}>
-                <Counter callback={() => { console.log('time is over!!!') }} initialTime={10}/>
-                <Box>score { score }</Box>
+                <Counter
+                    onFinishCount={handleOnFinish}
+                    initialCount={5}
+                />
+
+                <Box>score { score } </Box>
+                <Box>
+                    bool { swtchRef.current.toString() }
+                </Box>
             </Box>
             <Box className={'layout'}>
                 <Box className={'box-container'}>
-                    <Box className={'box active'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
-                    <Box className={'box'}/>
+                    <Box className={`box ${activeIndex === 0 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(0) }} />
+                    <Box className={`box ${activeIndex === 1 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(1) }} />
+                    <Box className={`box ${activeIndex === 2 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(2) }} />
+                    <Box className={`box ${activeIndex === 3 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(3) }} />
+                    <Box className={`box ${activeIndex === 4 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(4) }} />
+                    <Box className={`box ${activeIndex === 5 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(5) }} />
+                    <Box className={`box ${activeIndex === 6 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(6) }} />
+                    <Box className={`box ${activeIndex === 7 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(7) }} />
+                    <Box className={`box ${activeIndex === 8 ? 'active' : ''}`} onClick={() => { handleBoxOnClick(8) }} />
                 </Box>
             </Box>
         </CustomThemeProvider>
--- src/components/dum/counter/counter.tsx
+++ src/components/dum/counter/counter.tsx
@@ -2,32 +2,68 @@ import React, {useEffect, useState} from 'react';
 import {Box} from "@mui/material";
 
 interface CounterProps {
-    callback?: () => void;
-    initialTime?: number;
+    onFinishCount?: () => void;
+    onChangeCount?: (count: number) => void;
+    onStartCount?: (count: number) => void;
+    defaultInterval?: number;
+    initialCount?: number;
+    showText?: boolean;
+    reset?: boolean;
 }
 
 function Counter(props: CounterProps) {
-    const { callback, initialTime = 60 } = props
-    const [count, setCount] = useState(initialTime)
+    const {
+        onFinishCount,
+        onChangeCount,
+        initialCount = 60,
+        onStartCount,
+        defaultInterval = 1000,
+        showText = true,
+        reset = false
+    } = props
+    const [count, setCount] = useState(initialCount)
+
+    function handleOnStart() {
+        if (count === initialCount && onStartCount !== undefined) {
+            onStartCount(count)
+        }
+    }
 
     useEffect(() => {
         const timerId = setTimeout(() => {
             if (count > 0) {
                 setCount(count - 1)
-            } else if (count === 0 && callback !== undefined) {
-                callback()
+
+                if (onChangeCount !== undefined) {
+                    onChangeCount(count)
+                }
+            } else if (count === 0) {
+                if (onFinishCount !== undefined) {
+                    onFinishCount()
+                }
+
+                if (reset) {
+                    setCount(initialCount)
+                }
             }
-        }, 1000)
+        }, defaultInterval)
 
         return () => {
             clearTimeout(timerId)
         }
     }, [count])
 
+    useEffect(() => {
+        handleOnStart()
+    }, [])
+
     return (
-        <Box>
-            counter: {count}
-        </Box>
+        showText ?
+            <Box>
+                counter: {count}
+            </Box>
+            : null
+
     );
 }
 
